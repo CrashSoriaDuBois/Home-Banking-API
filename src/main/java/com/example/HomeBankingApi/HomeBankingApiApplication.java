a2ff729435a341391ba508a260108af949b0b6a6
Agustin Soria <soriapreacher@gmail.com>
2026-02-01 15:39:22 -0300

    Initial commit

---
+++ src/main/java/com/example/HomeBankingApi/HomeBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.HomeBankingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HomeBankingApi/HomeBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeBankingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
