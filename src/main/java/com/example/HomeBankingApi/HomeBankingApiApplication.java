9cc2abbfdb8dcda2f8674259729541e9c42274bf
Agustin Soria <soriapreacher@gmail.com>
2026-02-02 18:17:31 -0300

    feat: add client entity and repositories; build: downgrade Gradle and Spring Boot to improve compatibility

--- src/main/java/com/example/HomeBankingApi/HomeBankingApiApplication.java
+++ src/main/java/com/example/HomeBankingApi/HomeBankingApiApplication.java
@@ -1,4 +1,11 @@
 package com.example.HomeBankingApi;
+import com.example.HomeBankingApi.models.Client;
+import com.example.HomeBankingApi.repository.ClientRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,4 +17,13 @@ public class HomeBankingApiApplication {
 		SpringApplication.run(HomeBankingApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+			Client client1 = new Client(1, "Melba", "Morel", "melba@mindhub.com");
+			clientRepository.save(client1);
+
+			clientRepository.save(new Client(2, "Ines", "Risotto", "inessirotto@gmail.com"));
+		});
+	}
 }
---
+++ src/main/java/com/example/HomeBankingApi/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.example.HomeBankingApi.repository;
+
+import com.example.HomeBankingApi.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+}
