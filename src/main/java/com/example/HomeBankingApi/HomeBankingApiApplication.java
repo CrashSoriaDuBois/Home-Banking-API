44a17c2e5300998c468e674c21f77272fa91c713
Agustin Soria <soriapreacher@gmail.com>
2026-02-28 19:12:43 -0300

    fix: prevent creation of transactions without a linked account

--- src/main/java/com/example/HomeBankingApi/HomeBankingApiApplication.java
+++ src/main/java/com/example/HomeBankingApi/HomeBankingApiApplication.java
@@ -28,11 +28,11 @@ public class HomeBankingApiApplication {
 			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com");
 			Account account1 = new Account("VIN001", LocalDate.now(),5000 );
 			Account account2 = new Account("VIN002", (LocalDate.now()).plusDays(1),7500 );
-			Transaction transaction_1 = new Transaction(TransactionType.DEBIT,5000,"LALA", LocalDateTime.now());
+			Transaction transaction_1 = new Transaction(TransactionType.DEBIT,5000,"LALA", LocalDateTime.now(),account1);
 
 			client1.addAccounts(account1);
 			client1.addAccounts(account2);
-			account1.addTransaction(transaction_1);
+			//account1.addTransaction(transaction_1);
 
 			clientRepository.save(client1);
 			accountRepository.save(account1);
@@ -43,11 +43,11 @@ public class HomeBankingApiApplication {
 			Client client2 = new Client("Ines", "Risotto", "inessirotto@gmail.com");
 			Account account3 = new Account("VIN003", LocalDate.now(),2500 );
 			Account account4 = new Account("VIN004", (LocalDate.now()).plusDays(1),1000 );
-			Transaction transaction_2 = new Transaction(TransactionType.CREDIT,25000,"LALA",LocalDateTime.now());
+			Transaction transaction_2 = new Transaction(TransactionType.CREDIT,25000,"LALA",LocalDateTime.now(),account3);
 
 			client2.addAccounts(account3);
 			client2.addAccounts(account4);
-			account3.addTransaction(transaction_2);
+			//account3.addTransaction(transaction_2);
 
 			clientRepository.save(client2);
 			accountRepository.save(account3);
--- src/main/java/com/example/HomeBankingApi/models/Transaction.java
+++ src/main/java/com/example/HomeBankingApi/models/Transaction.java
@@ -25,11 +25,12 @@ public class Transaction {
     public Transaction() {
     }
 
-    public Transaction(TransactionType type, double amount, String description, LocalDateTime date) {
+    public Transaction(TransactionType type, double amount, String description, LocalDateTime date, Account account) {
         this.type = type;
         this.amount = amount;
         this.description = description;
         this.date = date;
+        this.account = account;
     }
 
     public Long getId() {
